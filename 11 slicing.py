10574acbf5e2eea3eb05d7f10e6a981848d8acdd
madan656 <kmmr656@gmail.com>
2024-06-14 14:26:40 +0530

    code updated

---
+++ 11 slicing.py
@@ -0,0 +1,22 @@
+# you can return a range of charecters by using the slice syatax
+
+b= "hello"
+print(b[2:5])
+
+# slice from the start 
+b= "hello"
+print(b[ :5])
+
+#slice to the end
+b= "hello"
+print(b[1: ])
+
+
+#negative indexing
+b= "hello"
+print(b[-3:-1])
+
+
+
+# a= ['apple','banana','cherry','dragonfruit','mango','graphs']
+# print(a[ : : 2])
