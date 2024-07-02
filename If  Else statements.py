094d13118eb8e54126b7b8bdb2db7af44486fc65
madan656 <kmmr656@gmail.com>
2024-07-02 07:43:37 +0530

    if...Else statement

---
+++ If  Else statements.py
@@ -0,0 +1,21 @@
+a = 33
+b= 300
+if b > a:
+    print("b is greaterthan a ")
+
+if a < b:
+    print("a is lessthan b ")
+
+c = 33
+d = 33
+
+if c > d:
+    print("c is greaterthan d")
+elif c < d:
+    print("c is not lessthan d")
+
+else :
+    print("c and d both are equal")
+
+# short hane if statements
+print( "c is less than d") if c > d else   print("c not less than d")
